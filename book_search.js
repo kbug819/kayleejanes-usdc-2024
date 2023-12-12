b637b304ccdbda15e8bc800e60fbe69ebfa79f08
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-12-11 19:20:43 -0600

    completed case sensitive testing to verify results

--- book_search.js
+++ book_search.js
@@ -97,6 +97,40 @@ const twentyLeaguesOut = {
  * Please add your unit tests below.
  * */
 
+/* Further test setup: */
+/* Test setup using 'and': */
+const twentyLeaguesTestAnd = {
+    "SearchTerm": "and",
+    "Results": [
+        {
+        "ISBN": "9780000528531",
+        "Page": 31,
+        "Line": 9
+    },         {
+        "ISBN": "9780000528531",
+        "Page": 31,
+        "Line": 10
+    }]
+}
+
+/* Test setup using 'nomatch': */
+const twentyLeaguesTestNomatch = {
+    "SearchTerm": "nomatch",
+    "Results": []
+}
+
+/* Test setup using 'The': */
+const twentyLeaguesTestThe = {
+    "SearchTerm": "The",
+    "Results": [
+        {
+            "ISBN": "9780000528531",
+            "Page": 31,
+            "Line": 8
+        }
+    ]
+}
+
 /** We can check that, given a known input, we get a known output. */
 const test1result = findSearchTermInBooks("the", twentyLeaguesIn);
 if (JSON.stringify(twentyLeaguesOut) === JSON.stringify(test1result)) {
@@ -117,6 +151,7 @@ if (test2result.Results.length == 1) {
     console.log("Received:", test2result.Results.length);
 }
 
+/** Positive test & verification of multiple results - Second check to verify positive results using 'and'. */
 const test3result = findSearchTermInBooks("and", twentyLeaguesIn); 
 if (test3result.Results.length == 2) {
     console.log("PASS: Test 3");
@@ -126,25 +161,62 @@ if (test3result.Results.length == 2) {
     console.log("Received:", test3result.Results.length);
 }
 
-const test4result = findSearchTermInBooks("nomatch", twentyLeaguesIn); 
-if (test4result.Results.length == 0) {
+/** Positive test & verification of multiple results - Length test using 'and' */
+const test4result = findSearchTermInBooks("and", twentyLeaguesIn); 
+if (JSON.stringify(twentyLeaguesTestAnd) === JSON.stringify(test4result)) {
     console.log("PASS: Test 4");
 } else {
     console.log("FAIL: Test 4");
-    console.log("Expected:", twentyLeaguesOut.Results.length);
-    console.log("Received:", test4result.Results.length);
+    console.log("Expected:", twentyLeaguesTestAnd);
+    console.log("Received:", test4result);
 }
 
-const twentyLeaguestestnomatch = {
-    "SearchTerm": "nomatch",
-    "Results": []
-}
-
-const test5result = findSearchTermInBooks("nomatch", twentyLeaguesIn);
-if (JSON.stringify(twentyLeaguestestnomatch) === JSON.stringify(test5result)) {
+/** Negative test - Verification that if no matches are returned, an empty array is present, and 0 matches */
+const test5result = findSearchTermInBooks("nomatch", twentyLeaguesIn); 
+if (test5result.Results.length == 0) {
     console.log("PASS: Test 5");
 } else {
     console.log("FAIL: Test 5");
-    console.log("Expected:", []);
-    console.log("Received:", test5result);
+    console.log("Expected:", twentyLeaguesOut.Results.length);
+    console.log("Received:", test5result.Results.length);
+}
+
+/** Negative test - Verification that if no matches are returned, an empty array is present, and 0 matches */
+const test6result = findSearchTermInBooks("nomatch", twentyLeaguesIn);
+if (JSON.stringify(twentyLeaguesTestNomatch) === JSON.stringify(test6result)) {
+    console.log("PASS: Test 6");
+} else {
+    console.log("FAIL: Test 6");
+    console.log("Expected:", twentyLeaguesTestNomatch);
+    console.log("Received:", test6result);
+}
+
+/** Case-sensitive test - Verification that 'The' returns a response */
+const test7result = findSearchTermInBooks("The", twentyLeaguesIn);
+if (JSON.stringify(twentyLeaguesTestThe) === JSON.stringify(test7result)) {
+    console.log("PASS: Test 7");
+} else {
+    console.log("FAIL: Test 7");
+    console.log("Expected:", twentyLeaguesTestThe);
+    console.log("Received:", test7result);
+}
+
+/** Case-sensitive test - Verification that 'The' returns a response with only 1 result */
+const test8result = findSearchTermInBooks("The", twentyLeaguesIn); 
+if (test8result.Results.length == 1) {
+    console.log("PASS: Test 8");
+} else {
+    console.log("FAIL: Test 8");
+    console.log("Expected:", twentyLeaguesTestThe.Results.length);
+    console.log("Received:", testresult.Results.length);
+}
+
+/** Case-sensitive test - Verification that responses for 'the' and 'The' are not the same and return different results */
+const test9result = findSearchTermInBooks("The", twentyLeaguesIn);
+if (JSON.stringify(test1result) !== JSON.stringify(test9result)) {
+    console.log("PASS: Test 9");
+} else {
+    console.log("FAIL: Test 9");
+    console.log("Expected:", twentyLeaguesTestThe);
+    console.log("Received:", twentyLeaguesOut);
 }
